e0874896b3567b4218be2337b1a4b46ba2050196
Mriga195 <mrigank195@gmail.com>
2025-09-04 15:47:05 +0530

    backend setup

---
+++ BACKEND/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const DB = process.env.DB_STRING.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => {
+    console.log("DB connection error:", err);
+    process.exit(1);
+  });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
